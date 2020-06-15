b27a502625ab77b45a408fd62236ac2301dee44c
Romina Velarde <rominapaola.velarde@gmail.com>
2020-06-15 04:03:25 +0000

    Done.

--- nested.rb
+++ nested.rb
@@ -15,7 +15,7 @@ def hopper
         :languages => ["C"]
       }
     }
-
+programmer_hash[:grace_hopper] 
 end
 
 def alan_kay_is_known_for
@@ -36,7 +36,7 @@ def alan_kay_is_known_for
         :languages => ["C"]
       }
     }
-
+programmer_hash[:alan_kay][:known_for] 
 end
 
 def dennis_ritchies_language
@@ -52,10 +52,10 @@ def dennis_ritchies_language
       },
       :dennis_ritchie => {
         :known_for => "Unix",
-        :languages => ["C"]
+        :languages => "C"
       }
     }
-
+programmer_hash[:dennis_ritchie][:languages]
 end
 
 def adding_matz
@@ -68,6 +68,10 @@ def adding_matz
 
 	programmer_hash = 
  		{
+ 		 :yukihiro_matsumoto => {
+     :known_for => "Ruby",
+    :languages => ["LISP", "C"]
+ },
       :grace_hopper => {
         :known_for => "COBOL",
         :languages => ["COBOL", "FORTRAN"]
@@ -95,7 +99,7 @@ def changing_alan
         :languages => ["COBOL", "FORTRAN"]
       },
       :alan_kay => {
-        :known_for => "Object Orientation",
+        :known_for => "GUI",
         :languages => ["Smalltalk", "LISP"]
       },
       :dennis_ritchie => {
@@ -125,5 +129,7 @@ def adding_to_dennis
         :languages => ["C"]
       }
     }
-
+  
+programmer_hash[:dennis_ritchie][:languages]<< "Assembly" 
+programmer_hash
 end
